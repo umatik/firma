fdf3d6cb4a9315ff25cb597cf55eee784ddfca79
fnowacki <hello@nowacki.it>
2018-01-10 20:30:53 +0100

    Dodanie listingu produktów

--- src/Product/Action/ListProductsAction.php
+++ src/Product/Action/ListProductsAction.php
@@ -4,13 +4,26 @@ declare(strict_types = 1);
 namespace App\Product\Action;
 
 use App\Common\Action\BaseAction;
+use App\Common\Domain\Service\MenuService;
+use App\Product\Domain\Model\ProductModel;
 use App\Product\Responder\ListProductsResponder;
 use Symfony\Component\HttpFoundation\Response;
 
 final class ListProductsAction extends BaseAction
 {
-    public function __invoke(ListProductsResponder $responder): Response
-    {
-        return $responder();
+    const PAGE_NAME = 'Produkty';
+
+    public function __invoke(
+        ListProductsResponder $responder,
+        MenuService $menuService,
+        ProductModel $productModel
+    ): Response {
+        $products = $productModel->listProducts();
+
+        return $responder([
+            'menuService' => $menuService,
+            'pageName' => self::PAGE_NAME,
+            'products' => $products
+        ]);
     }
 }
